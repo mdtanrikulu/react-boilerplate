ed914b55cd06244d3a813658862ebdb1f7c01e70
mdtanrikulu <md.tanrikulu@gmail.com>
2017-01-04 14:32:33 +0100

    combineReducers updated for react-router-redux conf

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import asyncLoad from './asyncLoad'
 import setSearch from './setSearch'
 import clickEvent from './clickEvent'
+import { routerReducer } from 'react-router-redux'
 
 const reducer = combineReducers({
     asyncLoad,
     setSearch,
-    clickEvent
+    clickEvent,
+    routing: routerReducer
 })
 
 export default reducer
